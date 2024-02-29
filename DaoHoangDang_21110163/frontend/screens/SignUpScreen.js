0bded03dc5489cf3a5471d38e87070040565ab6b
HoangDang1123 <110187412+HoangDang1123@users.noreply.github.com>
2024-02-29 15:13:58 +0700

    update homepage

---
+++ DaoHoangDang_21110163/frontend/screens/SignUpScreen.js
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from "react-native";
+
+export default function SignUpScreen() {
+  return(
+    <SafeAreaView>
+        <Text>SignUpScreen</Text>
+    </SafeAreaView>
+  )
+}
---
+++ DaoHoangDang_21110163/frontend/screens/index.js
@@ -0,0 +1,9 @@
+import LoginScreen from "./LoginScreen";
+import WelcomeScreen from "./WelcomeScreen";
+import SignUpScreen from "./SignUpScreen";
+
+export {
+    LoginScreen,
+    WelcomeScreen,
+    SignUpScreen
+}
